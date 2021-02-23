da4c995e737cd0af3017b212a2907d177be3c1f5
ybs32 <hdecogtrexi557960@gmail.com>
2021-02-24 02:03:06 +0900

    integration

--- opt/ybs32/bin/unico.sh
+++ opt/ybs32/bin/unico.sh
@@ -2,11 +2,13 @@
 
 mkdir -p /var/log/gunicorn
 
+# OPTIONS
 BIND="-b 0.0.0.0:8000"
-WORKERS="-w 1"
-CLASS="-k worker.CustomUvicornWorker"
+WORKER="-w 1 -k worker.CustomUvicornWorker"
 LOGGING="--log-config /etc/ybs32/gunicorn/logging.conf"
+PROC_NAME="--name unico" # for Datadog
+STATSD="--statsd-host 127.0.0.1:8125" # for Datadog
 
 # Run
 cd /opt/ybs32/src
-/root/.pyenv/shims/gunicorn unico:app $BIND $WORKERS $CLASS $LOGGING
+/root/.pyenv/shims/gunicorn unico:app $BIND $WORKER $LOGGING $PROC_NAME $STATSD
